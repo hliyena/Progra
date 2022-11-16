61dfd607cf4c59a1e585deb80b4b5c767b271104
Andrey <hliyena@yandex.ru>
2022-11-16 14:27:21 +0500

    Homework

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -20,3 +20,24 @@ else
         Console.WriteLine("Оба числа равны");
     }
 }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое число: ");
+int number3 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int number4 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите первое число: ");
+int number5 = int.Parse(Console.ReadLine()!);
+int max = number3;
+
+if (max < number4)
+{
+    max = number4;
+}
+if (max < number5)
+{
+    max = number5;
+}
+Console.WriteLine($"Максимальное число = {max}");
